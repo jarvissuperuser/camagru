3cff4f07a4fbc7dcb9539c26d0b5bf93cb931943
jarvissuperuser <mugadzatt001@gmail.com>
2016-10-23 14:16:40 +0200

    last commit 3

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ var dmO = {
 	cnv: null,
 	ecn: null,
 	feed: null,
+	img: null,
+	hpnl: null,
+	ic: null,
 	onload: function () {
 		dmO.cam = document.getElementById('cam');
 		dmO.ecn = document.getElementById('mix');
 		dmO.cnv = dmO.ecn.getContext('2d');
-
+		ic = 0;
 		if (navigator.webkitGetUserMedia)
 		{
 			dev = navigator.webkitGetUserMedia({video: true}, function (strm) {
@@ -28,8 +31,22 @@ var dmO = {
 			var h = dmO.cnv.height = dmO.cam.videoHeight;
 			var w = dmO.cnv.width = dmO.cam.videoWidth;
 			dmO.cnv.drawImage(dmO.cam, 0, 0, w, h, 0, 0, 300, 150);
+
 		}
 		//console.log(this.cnv, this.ecn);
+	},
+	save: function () {
+		//dmO.img = dmO.cnv.getImageData(0, 0, 320, 240);
+		//alert(JSON.stringify(dmO.img));
+		var pic = dmO.ecn.toDataURL('image/png', 1.0);
+		var im = document.createElement('img');
+		im.src = pic;
+		im.setAttribute('class', 'hi');
+		dmO.hpnl = document.getElementById('history');
+		dmO.hpnl.appendChild(im);
+		//ic = 0;
+		ic++;
+		console.log(ic);
 	}
 };
 
